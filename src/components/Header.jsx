b2c5ded3a7e795d7e053e1bc13a3a0d95e507618
sumitkumar785796 <76932212+sumitkumar785796@users.noreply.github.com>
2023-12-22 11:48:29 +0530

    first initi

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  const myStyle={
+    color:'white',
+    textAlign:'center',
+  }
+  return (
+    <div>
+        <h1 style={myStyle}>MOVIE TRAILERS</h1>
+    </div>
+  )
+}
+
+export default Header
