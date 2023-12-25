2f69132aa971ef46e520f72ef4308a211919fc60
sumitkumar785796 <76932212+sumitkumar785796@users.noreply.github.com>
2023-12-25 16:41:50 +0530

    update some code

--- src/components/Fun.jsx
+++ src/components/Fun.jsx
@@ -7,7 +7,7 @@ const Fun = ({name,img,writer}) => {
     <article className="hentry">
     <header className="entry-header">
         <div className="entry-thumbnail">
-        <a href="portfolio-item.html"><img src={img} className="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="p1" /></a>
+        <a href=""><img src={img} className="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt={name} /></a>
         </div>
         <h2 className="entry-title">{name}</h2>
         <p className="portfoliotype" >{writer}</p>
